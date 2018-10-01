0fcf382f514e85d0d3861aa4b10091ee32abd742
Rubina Adhikari <arubina@vt.edu>
2018-10-01 09:53:51 -0400

    Update sol3.py

--- project2/sol3.py
+++ project2/sol3.py
@@ -5,8 +5,8 @@ from shellcode import shellcode
 #toReturnSC = 0xbffe7c18
 #toReturn = 0xbffe842c
 
-toReturnSC = 0xbffe7c18
-toReturn = 0xbffe8428
+toReturnSC = 0xbffe7408
+toReturn = 0xbffe7c14
 
 #print shellcode
 
